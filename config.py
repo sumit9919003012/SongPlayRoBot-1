35575c0b936fe6c3cf601ed44b4baed3ef3cab89
Sumit Kumar Chaurasiya <94757093+sumit9919003012@users.noreply.github.com>
2023-05-21 17:00:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
     API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    BOT_TOKEN = "" # 🤖 Get It From @BotFather  By Sending /newbot Command.
+  
